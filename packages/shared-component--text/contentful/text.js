d03509d82b47fc7b3b4b03fb1c58ccb071c1fe05
Gurpreet Badyal <gupreet.baydal@coop.co.uk>
2020-04-07 10:10:59 +0100

    add contentful script for text content type

---
+++ packages/shared-component--text/contentful/text.js
@@ -0,0 +1,33 @@
+module.exports = function (migration) {
+  const text = migration.createContentType('text');
+  text.name('-- Text')
+    .description('Use to add a block of body text.')
+
+  text.createField('name')
+    .name('Name')
+    .type('Symbol')
+    .required(true)
+
+  text.createField('text')
+    .name('Text')
+    .type('Text')
+    .required(true)
+
+  text.displayField('name');
+
+  text.changeFieldControl(
+    'name',
+    'builtin',
+    'singleLine',
+    { helpText: 'This is the name of the component in Contentful. It will not display on the website.' }
+  )
+
+  text.changeFieldControl(
+    'text',
+    'builtin',
+    'markdown',
+    { helpText: 'Add plain text and format it using Markdown or the options on the bar above the text box. Embedded images are rendered at actual size and should be no wider than 680px.' }
+  )
+
+}
+
