f18454f3245ecd63a100e4271741a3bee280f7d8
Colin Rotherham <work@colinr.com>
2020-06-08 14:30:01 +0100

    Add sourcemaps to Design System CSS

--- design-system/gulpfile.js
+++ design-system/gulpfile.js
@@ -123,6 +123,7 @@ function html() {
 function css() {
   return gulp
     .src(src_paths.css)
+    .pipe(sourcemaps.init())
     .pipe(cssimport(importOptions))
     .pipe(
       postcss(
@@ -135,6 +136,7 @@ function css() {
       )
     )
     .pipe(autoprefixer())
+    .pipe(sourcemaps.write('maps/'))
     .pipe(gulp.dest(dest_paths.styles))
     .pipe(connect.reload());
 }
