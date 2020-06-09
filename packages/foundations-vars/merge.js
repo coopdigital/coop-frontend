9194d2be30871943bad52e9012df10a02e72af77
Matt Tyas <matthewtyas@hotmail.com>
2020-06-09 12:41:31 +0100

    new tracking code

--- packages/foundations-vars/merge.js
+++ packages/foundations-vars/merge.js
@@ -1,2 +1,2 @@
 var combineFiles = require('combine-files');
-combineFiles(['./dist/css/_vars.css', './src/media/breakpoints.css'], './dist/vars.css', '\n\n');
+combineFiles(['./dist/css/_vars.css', './packages/foundations-vars/src/media/breakpoints.css'], './dist/vars.css', '\n\n');
