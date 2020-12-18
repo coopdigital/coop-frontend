204ba5e5caa4eec71cd617554679d6ccf97d1def
Chris Gibbons <chris.gibbons@coop.co.uk>
2020-12-18 14:54:10 +0000

    Initial commit of design tokens work

---
+++ packages/design-tokens/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+// Do not edit directly
+// Generated on Fri, 18 Dec 2020 14:50:23 GMT
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
