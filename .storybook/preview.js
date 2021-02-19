0786a2c568a446e012ac1d292465a5cc645d4096
Dale Hudson <dalefrontend@gmail.com>
2021-02-19 12:12:41 +0000

    add foundations vars, global and typogrpahy as dependencies to use in storybook preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import "@coopdigital/foundations-global";
+import "@coopdigital/foundations-vars";
+import "@coopdigital/foundations-typography";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
