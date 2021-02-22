2461205348aab8c0b4f6da6b40b9ef2df825a3cb
Dale Hudson <dalefrontend@gmail.com>
2021-02-22 13:36:23 +0000

    rename extension from js to jsx for blockquote component

---
+++ packages/component-blockquote/src/BlockQuote.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './blockquote.css';
+
+const BlockQuote = ({ className, citation, children }) => (
+  <blockquote>
+    <p className={className}>{children}</p>
+    <cite>{citation}</cite>
+  </blockquote>
+);
+
+BlockQuote.defaultProps = {
+  className: null,
+  citation: null,
+  children: null,
+};
+
+BlockQuote.propTypes = {
+  className: PropTypes.string,
+  citation: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default BlockQuote;
