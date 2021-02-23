12aad30480fc1b25b76b5751a5379d1bb650418e
Dale Hudson <dalefrontend@gmail.com>
2021-02-23 14:03:23 +0000

    update import css to pcss in BlockQuote component

--- packages/component-blockquote/src/BlockQuote.jsx
+++ packages/component-blockquote/src/BlockQuote.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './blockquote.css';
+import './blockquote.pcss';
 
 const BlockQuote = ({ className, citation, children }) => (
   <blockquote>
