fbf412b6418a399b328050f590c734f518744669
Colin Rotherham <work@colinr.com>
2021-02-25 10:35:59 +0000

    Match parent Babel config

--- design-system/babel.config.js
+++ design-system/babel.config.js
@@ -3,7 +3,8 @@ module.exports = (api) => {
 
   const presets = [
     ['@babel/preset-env', {
-      corejs: 3,
+      bugfixes: true,
+      corejs: '3.9',
       loose: true,
       shippedProposals: true,
       useBuiltIns: 'usage',
