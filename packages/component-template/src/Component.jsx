6c6f65eedc66adca51bc5c4d5c0f258886569ace
Dale Hudson <dalefrontend@gmail.com>
2021-03-02 14:28:52 +0000

    update: start component-template package updates with react and storybook

---
+++ packages/component-template/src/Component.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './component.pcss';
+
+// Example react component with 3 props, using prop-types for type management.
+const Component = ({ className, heading, content }) => (
+  <article>
+    <h3 className={className}>{heading}</h3>
+    <div dangerouslySetInnerHTML={{__html: content }} />
+  </article>
+);
+
+Component.defaultProps = {
+  className: null,
+  heading: null,
+  content: null,
+};
+
+Component.propTypes = {
+  className: PropTypes.string,
+  heading: PropTypes.string,
+  content: PropTypes.node,
+};
+
+export default Component;
