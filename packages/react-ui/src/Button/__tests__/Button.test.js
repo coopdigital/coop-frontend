8dbe291860b7ba6480f6b724778fa027bafdf032
Dale Hudson <dalefrontend@gmail.com>
2021-06-16 17:23:41 +0100

    chore: lint Button test file

--- packages/react-ui/src/Button/__tests__/Button.test.js
+++ packages/react-ui/src/Button/__tests__/Button.test.js
@@ -3,20 +3,20 @@
  */
 
 import React from "react";
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import Button from "../src/index";
 
 afterEach(cleanup);
 
 describe("Button", () => {
   it("should render correctly", () => {
-    const {getByText} = render(<Button>Button rendered</Button>);
-    expect(getByText('Button rendered')).toBeInTheDocument();
+    const { getByText } = render(<Button>Button rendered</Button>);
+    expect(getByText("Button rendered")).toBeInTheDocument();
   });
 
   it("should support all variants", () => {
-    const expectedButtonText = ['Default', 'Primary', 'White', 'Grey'];
+    const expectedButtonText = ["Default", "Primary", "White", "Grey"];
     const Variants = () => (
       <div>
         <Button>Default</Button>
@@ -27,16 +27,17 @@ describe("Button", () => {
     );
     const { getByText, asFragment } = render(<Variants />);
 
-    expectedButtonText.forEach( expectedText => {
+    expectedButtonText.forEach((expectedText) => {
       expect(getByText(expectedText)).toBeInTheDocument();
     });
 
     const buttonVariantBlock = asFragment();
+    console.dir(buttonVariantBlock);
     expect(buttonVariantBlock).toMatchSnapshot();
   });
 
   it("should support all sizes", () => {
-    const expectedButtonText = ['Small', 'Default/Medium'];
+    const expectedButtonText = ["Small", "Default/Medium"];
 
     const Sizes = () => (
       <div>
@@ -45,44 +46,46 @@ describe("Button", () => {
       </div>
     );
 
-    const {getByText, asFragment } = render(<Sizes />);
+    const { getByText, asFragment } = render(<Sizes />);
 
-    expectedButtonText.forEach( expectedText => {
+    expectedButtonText.forEach((expectedText) => {
       expect(getByText(expectedText)).toBeInTheDocument();
     });
 
-    const buttonSizesBlock = asFragment()
+    const buttonSizesBlock = asFragment();
     expect(buttonSizesBlock).toMatchSnapshot();
   });
 
   it("should render different text", () => {
     const { rerender, getByText } = render(<Button>button</Button>);
-    expect(getByText('button')).toBeInTheDocument();
-
+    expect(getByText("button")).toBeInTheDocument();
 
     rerender(<Button>New text</Button>);
-    expect(getByText('New text')).toBeInTheDocument();
+    expect(getByText("New text")).toBeInTheDocument();
   });
 
-  it('should support full width buttons', () => {
-    const {container, asFragment} = render(<Button fullWidth>Full width button</Button>);
+  it("should support full width buttons", () => {
+    const { container, asFragment } = render(
+      <Button fullWidth>Full width button</Button>
+    );
     const fullwidthButton = asFragment();
 
-    expect(container.firstChild).toHaveClass('coop-btn--full-width')
+    expect(container.firstChild).toHaveClass("coop-btn--full-width");
     expect(fullwidthButton).toMatchSnapshot();
   });
 
-  it('should support loading state button prop', () => {
-    const {container, asFragment} = render(<Button isLoading>Button is loading</Button>);
+  it("should support loading state button prop", () => {
+    const { container, asFragment } = render(
+      <Button isLoading>Button is loading</Button>
+    );
     const loadingButton = asFragment();
 
-    expect(container.firstChild).toHaveClass('coop-btn--loading');
+    expect(container.firstChild).toHaveClass("coop-btn--loading");
     expect(loadingButton).toMatchSnapshot();
-  })
-
+  });
 
   it("should render empty button correctly", () => {
-    const {container, asFragment} = render(<Button />);
+    const { container, asFragment } = render(<Button />);
     const emptyButtonBlock = asFragment();
 
     expect(emptyButtonBlock).toMatchSnapshot();
