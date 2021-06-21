34f6ecc4fa983380f45993252ee960ca66d39ad6
Dale Hudson <dalefrontend@gmail.com>
2021-06-21 11:22:59 +0100

    move over Text component and restructure directory

---
+++ packages/react-ui/src/Text/src/Text.js
@@ -0,0 +1,54 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "../../utils/classNames";
+import "./text.scss";
+
+const variantList = {
+  mega: "coop-t-h-mega",
+  lead: "coop-t-lead-p",
+  regular: "coop-t-regular",
+  medium: "coop-t-medium",
+  bold: "coop-t-bold",
+};
+
+const Text = ({ className, variant, type, children, ...props }) => {
+  const TagName = type;
+  const variantClassName = classNames(variantList[variant], [className]);
+
+  const tagAttributes = {
+    className: variantClassName,
+    ...props,
+  };
+
+  return <TagName {...tagAttributes}>{children}</TagName>;
+};
+
+Text.defaultProps = {
+  type: "p",
+  variant: "regular",
+  className: null,
+  children: null,
+};
+
+Text.propTypes = {
+  /** className -  */
+  className: PropTypes.string,
+  /** variant - adds class variants to the text component. Variant props listed and the corresponding class name they use are:
+
+  - `mega: "coop-t-h-mega"`,
+  - `lead: "coop-t-lead-p"`,
+  - `regular: "coop-t-regular"`,
+  - `medium: "coop-t-medium"`,
+  - `bold: "coop-t-bold"`,
+   */
+
+  variant: PropTypes.oneOf(Object.keys(variantList)),
+  /** type -  the type of text element you want to render using the Text component. Includes:
+   * `h1`, `h2`, `h3`, `h4`, `h5`, `h6`, and `p` tags.
+  */
+  type: PropTypes.oneOf(["h1", "h2", "h3", "h4", "h5", "h6", "p"]),
+  /** children - prop that accepts a string or node? */
+  children: PropTypes.node,
+};
+
+export default Text;
