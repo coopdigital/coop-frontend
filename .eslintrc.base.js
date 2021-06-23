4e859e88379b6d3872c9c6d0a3cc004c94fc3ef3
Paul Le <paul.le@bjss.com>
2021-06-23 12:44:28 +0100

    COOP-1551 - revert top level eslint

--- .eslintrc.base.js
+++ .eslintrc.base.js
@@ -4,15 +4,15 @@ module.exports = {
     browser: true,
     node: true,
   },
-  extends: ['airbnb-base', 'plugin:jest/recommended'],
+  extends: ["airbnb-base", "plugin:jest/recommended"],
   parserOptions: {
     ecmaVersion: 12,
   },
-  plugins: ['jest'],
+  plugins: ["jest"],
   rules: {
-    'import/extensions': [
-      'error',
-      'always',
+    "import/extensions": [
+      "error",
+      "always",
       {
         ignorePackages: true,
       },
