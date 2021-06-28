3226e65bc1bca096dd029dc875cc5f82c97fa6ab
Dale Hudson <dalefrontend@gmail.com>
2021-06-28 13:49:39 +0100

    test: update Grid tests to remove enzyme, react-test-renderer andsnapshot tests

--- packages/react-ui/src/Grid/__tests__/Grid.test.js
+++ packages/react-ui/src/Grid/__tests__/Grid.test.js
@@ -1,31 +1,44 @@
 import React from "react";
-import { mount } from "enzyme";
-import renderer from "react-test-renderer";
+// import { mount } from "enzyme";
+// import renderer from "react-test-renderer";
+
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import Grid from "../src/index";
+
+afterEach(cleanup);
 
 describe("Grid", () => {
   it("should render correctly", () => {
-    const wrapper = mount(<Grid />);
-    expect(() => wrapper.unmount()).not.toThrow();
+    const { container } = render(<Grid />);
 
-    const tree = renderer.create(<Grid />).toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(container.firstChild).toHaveClass("coop-l-grid");
   });
 
   it("should allow center and reverse attributes", () => {
     const Attributes = () => (
       <>
-        <Grid center />
-        <Grid reverse />
-        <Grid center reverse />
+        <Grid data-testid="grid-test" center />
+        <Grid data-testid="grid-test" reverse />
+        <Grid data-testid="grid-test" center reverse />
       </>
     );
-    const tree = renderer.create(<Attributes />).toJSON();
-    expect(tree).toMatchSnapshot();
+
+    const { getAllByTestId } = render(<Attributes />);
+    const gridArray = getAllByTestId("grid-test");
+
+    expect(gridArray[0]).toHaveClass("coop-u-flex-center");
+    expect(gridArray[1]).toHaveClass("coop-u-flex-row-reverse");
+    expect(gridArray[2]).toHaveClass("coop-u-flex-center");
+    expect(gridArray[2]).toHaveClass("coop-u-flex-row-reverse");
+    for (let i = 0; i < gridArray.length; i++) {
+      expect(gridArray[i]).toHaveClass("coop-l-grid");
+    }
   });
 
   it("should render with children", () => {
-    const tree = renderer.create(<Grid>test</Grid>).toJSON();
-    expect(tree).toMatchSnapshot();
+    const testString = "test grid children";
+    const { getByText } = render(<Grid children={testString} />);
+    expect(getByText(testString)).toBeInTheDocument();
   });
 });
