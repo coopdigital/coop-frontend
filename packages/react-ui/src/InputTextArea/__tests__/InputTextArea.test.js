b4106ecd7da786aab50a9651cc85dae8bdb79871
Dale Hudson <dalefrontend@gmail.com>
2021-06-30 14:13:07 +0100

    tests: update tests and remove snapshot testing

--- packages/react-ui/src/InputTextArea/__tests__/InputTextArea.test.js
+++ packages/react-ui/src/InputTextArea/__tests__/InputTextArea.test.js
@@ -1,55 +1,73 @@
 import React from "react";
-import { mount } from "enzyme";
-import renderer from "react-test-renderer";
-import InputTextArea from "../index";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import InputTextArea from "../src/";
+
+afterEach(cleanup);
 
 describe("InputTextArea", () => {
   it("should render correctly", () => {
-    const wrapper = mount(<InputTextArea id="test" name="test" label="test" />);
-    expect(() => wrapper.unmount()).not.toThrow();
+    const { getByLabelText } = render(
+      <InputTextArea id="test" name="test" label="test" />
+    );
+    expect(getByLabelText("test")).toBeInTheDocument();
   });
 
   it("should support all variants", () => {
     const Variants = () => (
       <div>
         <InputTextArea id="label" name="label" label="Input with label" />
-        <InputTextArea id="label" name="label" hint="Input with hint" />
         <InputTextArea
-          id="label"
-          name="label"
+          data-testid="withHint"
+          id="withHint"
+          name="withHint"
+          hint="Input with hint"
+        />
+        <InputTextArea
+          data-testid="withLabelAndHint"
+          id="withLabelAndHint"
+          name="withLabelAndHint"
           label="Input with label and hint"
           hint="Input with label and hint"
         />
       </div>
     );
 
-    const tree = renderer.create(<Variants />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const { container, getAllLabelText, getByTestId } = render(<Variants />);
+    expect(container.getElementsByTagName("textarea").length).toBe(3);
 
-    const wrapper = mount(<Variants />);
-    expect(() => wrapper.unmount()).not.toThrow();
+    expect(getByTestId("withHint")).toHaveAccessibleDescription(
+      "Input with hint"
+    );
+
+    expect(getByTestId("withLabelAndHint")).toHaveAccessibleDescription(
+      "Input with label and hint"
+    );
   });
 
   it("should support all variants with error", () => {
     const VariantsWithError = () => (
       <div>
         <InputTextArea
-          id="label"
-          name="label"
+          data-testid="withLabelAndError"
+          id="withLabelAndError"
+          name="withLabelAndError"
           label="Input with label"
           errorMsg="Error message"
           hasError
         />
         <InputTextArea
-          id="label"
-          name="label"
+          data-testid="withHintAndError"
+          id="withHintAndError"
+          name="withHintAndError"
           hint="Input with hint"
           errorMsg="Error message"
           hasError
         />
         <InputTextArea
-          id="label"
-          name="label"
+          data-testid="withLabelHintAndError"
+          id="withLabelHintAndError"
+          name="withLabelHintAndError"
           label="Input with label and hint"
           hint="Input with label and hint"
           errorMsg="Error message"
@@ -58,10 +76,27 @@ describe("InputTextArea", () => {
       </div>
     );
 
-    const tree = renderer.create(<VariantsWithError />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const { container, getByTestId } = render(<VariantsWithError />);
+    const errorMessages = container.getElementsByClassName("coop-form__error");
 
-    const wrapper = mount(<VariantsWithError />);
-    expect(() => wrapper.unmount()).not.toThrow();
+    const errorInputs = container.getElementsByClassName("coop-form__invalid");
+    expect(errorInputs.length).toBe(3);
+
+    expect(getByTestId("withLabelAndError")).toHaveAccessibleDescription(
+      "Error message"
+    );
+
+    expect(getByTestId("withHintAndError")).toHaveAccessibleDescription(
+      "Input with hint Error message"
+    );
+
+    expect(getByTestId("withLabelHintAndError")).toHaveAccessibleDescription(
+      "Input with label and hint Error message"
+    );
+
+    expect(errorMessages.length).toBe(3);
+    for (let i = 0; i < errorMessages.length; i++) {
+      expect(errorMessages[i]).toHaveTextContent("Error message");
+    }
   });
 });
