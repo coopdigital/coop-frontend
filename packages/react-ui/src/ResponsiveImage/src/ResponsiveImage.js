e217d6541e5a0a80cb0b57dfa6b3a96a12446c2a
Dale Hudson <dalefrontend@gmail.com>
2021-06-30 11:44:37 +0100

    chore: remove stylesheet and import for image component

--- packages/react-ui/src/ResponsiveImage/src/ResponsiveImage.js
+++ packages/react-ui/src/ResponsiveImage/src/ResponsiveImage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import shortid from "shortid";
-// import "./responsiveImage.scss";
 
 const generateSourceMap = (sources) =>
   sources.map((source) => {
