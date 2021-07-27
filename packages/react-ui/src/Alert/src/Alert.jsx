6b9b36d6245bba5e59ebc24230482583e2c46e66
Michael Chadwick <michael.chadwick@coopdigital.co.uk>
2021-07-27 17:16:49 +0100

    refactor: move Alert to use dedicated props for values

--- packages/react-ui/src/Alert/src/Alert.jsx
+++ packages/react-ui/src/Alert/src/Alert.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "../../utils/classNames";
 
-const Alert = ({ type, heading, link, children, testId }) => {
-  const { href, text } = link;
-
+const Alert = ({ title, text, link, type, testId }) => {
   const classes = classNames("coop-c-notification", [
     type === "warn" && "coop-c-notification--alert",
     type === "error" && "coop-c-notification--error",
-    type === "success" && "coop-c-notification--success",
+    type === "success" && "coop-c-notification--success"
   ]);
 
   const tagAttributes = {
     className: classes,
-    role: type === "warn" || type === "error" ? "alert" : "status",
+    role: type === "warn" || type === "error" ? "alert" : "status"
   };
 
   return (
@@ -22,37 +20,30 @@ const Alert = ({ type, heading, link, children, testId }) => {
       className={tagAttributes.className}
       role={tagAttributes.role}
     >
-      <h3 className="coop-c-notification__heading">{heading}</h3>
+      <h3 className="coop-c-notification__heading">{title}</h3>
       <p className="coop-c-notification__p">
-        {href && (
-          <a href={href} className="coop-c-notification__link">
+        {link ? (
+          <a href={link} className="coop-c-notification__link">
             {text}
           </a>
+        ) : (
+          text
         )}
       </p>
-      {children && <p className="coop-c-notification__p">{children}</p>}
     </div>
   );
 };
 
 Alert.defaultProps = {
-  type: "info",
-  link: {
-    href: "",
-    text: "",
-  },
-  children: null,
+  type: "info"
 };
 
 Alert.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  link: PropTypes.string,
   type: PropTypes.oneOf(["info", "warn", "error", "success"]),
-  heading: PropTypes.string.isRequired,
-  link: PropTypes.shape({
-    href: PropTypes.string,
-    text: PropTypes.string,
-  }),
-  children: PropTypes.node,
-  testId: PropTypes.string,
+  testId: PropTypes.string
 };
 
 export default Alert;
