c9af8971014aaa1ff89eace4ac78826830ea1e14
matt-tyas <matt.tyas@gmail.com>
2021-10-13 12:17:03 +0100

    added icons package for crit

---
+++ packages/foundations-icons/postcss.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+
+module.exports = {
+  map: {
+    inline: false,
+  },
+  plugins: [
+    require('postcss-import'),
+    require('postcss-custom-media'),
+    require('postcss-custom-properties'),
+    require('postcss-nesting'),
+    require('postcss-calc'),
+    require('autoprefixer'),
+    require('cssnano'),
+  ],
+};
---
+++ packages/foundations-icons/readme.md
@@ -0,0 +1,20 @@
+# Co-op Foundations: Icons
+Icons as separate SVGs combined into a sprite sheet
+
+## Dependencies
+- `svg-sprite`
+
+## How to use
+Install via `npm`:
+```bash
+$ npm install @coopdigital/foundations-icons --save
+```
+
+## Examples
+
+
+## License
+Copyright (c) 2021 Co-operative Group Limited.
+Licensed [MIT](https://github.com/coopdigital/coop-frontend/blob/master/LICENSE).
+
+ 
