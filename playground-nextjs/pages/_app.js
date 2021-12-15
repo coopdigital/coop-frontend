dfe035e8a7e90f44da0c9b6f462badc411583284
Dale Hudson <dalefrontend@gmail.com>
2021-12-15 11:07:46 +0000

    Add nextjs for testing import of built editorialcard
    to test npm link editorialcard package to playground-nextjs directory
    
    NextJS is opinionated about how we import css.  A normal .css file imported
    into editorialcard react component will cause errors as NextJS requires you
    to either import the .css within _app.js as a global css file or you can
    use .module.css imports into the react component so NextJs will compile the
    styles for us.
    
    more info here: https://nextjs.org/docs/messages/css-global

---
+++ playground-nextjs/pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import '@coopdigital/component-editorial-card/dist/css/editorialCard.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
