a6a8565abf387e31d4d09c818dd4027e5f487b98
Dale Hudson <dalefrontend@gmail.com>
2021-12-20 16:01:14 +0000

    feat: start splitting out config and conditionally add bundle configs
    split out configs for html, pcss, js
    if directory and file globs match then add the relevant bundle config to
    the rollup default export
    
    TODO: move configs into separate files and import and add to config
    based on whether the src dir and extensions require the config. Would
    this help better manage each bundle config?
    
    TODO: tidy the conditionals, maybe spead and inline into and empty export default array?
    
    TODO: Do we need separate bundle configs for js and jsx, should we bundle all js/jsx into index.[cjs,mjs].js files.  Always having a index.js at the package src/ root to handle exporting all package js as named exports?

--- rollup.config.js
+++ rollup.config.js
@@ -1,86 +1,222 @@
-import rollup from 'rollup';
-import path from 'path'
+import path from 'path';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
-import copy from 'rollup-plugin-copy'
-import cleaner from 'rollup-plugin-cleaner'
+import copy from 'rollup-plugin-copy';
+import cleaner from 'rollup-plugin-cleaner';
 import rename from 'rollup-plugin-rename';
 import postcss from 'rollup-plugin-postcss';
-
+import htmlEntry from 'rollup-plugin-html-entry';
+import glob from 'glob';
 
 const currentWorkingPath = process.cwd();
-const { src, name, style, styleSrc } = require(path.join(currentWorkingPath, 'package.json'));
+const { src, name, style, styleSrc } = require(path.join(
+  currentWorkingPath,
+  'package.json'
+));
 
+const srcPath = `${currentWorkingPath}/src/`;
 const inputPathJS = path.join(currentWorkingPath, src);
 const inputPathCSS = path.join(currentWorkingPath, styleSrc);
-// const inputPathHTML = path.join(currentWorkingPath, src);
+const inputPathHTML = path.join(currentWorkingPath, 'src/html/**.html');
+const inputPathJINJA = path.join(currentWorkingPath, 'src/jinja/*.html');
 
-const packageRegEx = /@coopdigital\/component-|@coopdigital\/foundations-/;
 
-export default [
+const hasHTML = glob.sync(srcPath + '**/*.html').length > 0;
+const hasJINJA = glob.sync(srcPath + 'jinja/*.html').length > 0;
+const hasPCSS = glob.sync(srcPath + '**/*.html').length > 0;
+const hasJSX = glob.sync(srcPath + '**/index.mjs').length > 0;
+const hasIndexJS = glob.sync(srcPath + '**/index.mjs').length > 0;
 
+const exportedConfig = [];
+
+const configJSX = {};
+
+const configJS = {
+  input: inputPathJS,
+  external: ['react', 'prop-types', /\.pcss$/],
+  plugins: [
+    resolve({
+      extensions: ['.mjs', '.cjs', '.js', '.jsx'],
+    }),
+    cleaner({
+      targets: ['./dist/index.[cjs,mjs].js'],
+    }),
+    babel({
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      babelHelpers: 'bundled',
+      exclude: ['node_modules/**', '**/*.[test,spec].js'],
+    }),
+  ],
+  output: [
     {
-        input: inputPathJS,
-        external: ['react', 'prop-types', /\.pcss$/],
-        plugins: [
-            resolve({
-                extensions: ['.mjs', '.js', '.jsx', '.json', '.pcss', '.css'],
-              }),
-              cleaner({
-                targets: [
-                  './dist/index.[cjs,mjs].js',
-                ],
-              }),
-              babel({
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                babelHelpers: 'bundled',
-                exclude: 'node_modules/**',
-              }),
-        ],
-        output:[
-            {
-                file: 'dist/index.cjs.js',
-                format: 'cjs',
-                plugins: [
-                  rename({
-                    map: (name) => name.replace('.pcss', '.css'),
-                  }),
-                ],
-              },
-              {
-                file: 'dist/index.esm.js',
-                format: 'esm',
-                plugins: [
-                  rename({
-                    map: (name) => name.replace('.pcss', '.css'),
-                  }),
-                ],
-              },
-        ],
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      plugins: [
+        rename({
+          map: (name) => name.replace('.pcss', '.css'),
+        }),
+      ],
     },
     {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      plugins: [
+        rename({
+          map: (name) => name.replace('.pcss', '.css'),
+        }),
+      ],
+    },
+  ],
+};
 
-        input: inputPathCSS,
-        plugins: [
-            cleaner({
-              targets: ['./dist/css'],
-            }),
-            postcss({
-              config: {
-                path: '../../postcss.config.js',
-              },
-              sourceMap: true,
-              extensions: ['.pcss', '.css'],
-              inject: false,
-              modules: false,
-              extract: path.resolve(style),
-            }),
-          ],
-        output: [
-            {
-                file: style,
-                format: 'esm',
-            }
-        ]
-    }
-]
+const configPCSS = {
+  input: inputPathCSS,
+  plugins: [
+    cleaner({
+      targets: ['./dist/css'],
+    }),
+    postcss({
+      config: {
+        path: '../../postcss.config.js',
+      },
+      sourceMap: true,
+      extensions: ['.pcss', '.css'],
+      inject: false,
+      modules: false,
+      extract: path.resolve(style),
+    }),
+  ],
+  output: [
+    {
+      file: style,
+      format: 'esm',
+    },
+  ],
+};
+
+const configHTML = {
+  input: 'src/html/*.html',
+  plugins: [
+    htmlEntry({ exports: false }),
+    cleaner({
+      targets: [
+        './dist/html/',
+        './dist/jinja/',
+      ],
+    }),
+    copy(
+      {
+        targets: [
+          { src: 'src/html/**.html', dest: 'dist/html' },
+        ],
+      },
+    ),
+  ],
+};
+
+const configJINJA = {
+  input: 'src/jinja/*.html',
+  plugins: [
+    htmlEntry({ exports: false }),
+    cleaner({
+      targets: [
+        './dist/jinja/',
+      ],
+    }),
+    copy(
+      {
+        targets: [
+          { src: 'src/jinja/**.html', dest: 'dist/jinja' },
+        ],
+      },
+    ),
+  ],
+};
+
+
+//load config/add config to config array used in default export
+/* Idea: possibly look up both jinja and html templates here
+       or split out conditionals to check for jinja and html */
+if(hasHTML) {
+  exportedConfig.push(configHTML)
+}
+
+if(hasJINJA) {
+  exportedConfig.push(configJINJA)
+}
+
+if(hasPCSS) {
+  exportedConfig.push(configPCSS)
+}
+
+if(hasIndexJS) {
+  exportedConfig.push(configJS)
+} else if(hasJSX) {
+  // exportedConfig.push(configJSX)
+}
+
+export default exportedConfig;
+
+// [
+//   {
+//     input: inputPathJS,
+//     external: ['react', 'prop-types', /\.pcss$/],
+//     plugins: [
+//       resolve({
+//         extensions: ['.mjs', '.cjs', '.js', '.jsx'],
+//       }),
+//       cleaner({
+//         targets: ['./dist/index.[cjs,mjs].js'],
+//       }),
+//       babel({
+//         presets: ['@babel/preset-env', '@babel/preset-react'],
+//         babelHelpers: 'bundled',
+//         exclude: ['node_modules/**', '**/*.[test,spec].js'],
+//       }),
+//     ],
+//     output: [
+//       {
+//         file: 'dist/index.cjs.js',
+//         format: 'cjs',
+//         plugins: [
+//           rename({
+//             map: (name) => name.replace('.pcss', '.css'),
+//           }),
+//         ],
+//       },
+//       {
+//         file: 'dist/index.esm.js',
+//         format: 'esm',
+//         plugins: [
+//           rename({
+//             map: (name) => name.replace('.pcss', '.css'),
+//           }),
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     input: inputPathCSS,
+//     plugins: [
+//       cleaner({
+//         targets: ['./dist/css'],
+//       }),
+//       postcss({
+//         config: {
+//           path: '../../postcss.config.js',
+//         },
+//         sourceMap: true,
+//         extensions: ['.pcss', '.css'],
+//         inject: false,
+//         modules: false,
+//         extract: path.resolve(style),
+//       }),
+//     ],
+//     output: [
+//       {
+//         file: style,
+//         format: 'esm',
+//       },
+//     ],
+//   },
+// ];
