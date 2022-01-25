4c5af0418ce5881bb61b9a55869a47a73770af7c
MBLHarrison <mitchellblharrison@gmail.com>
2022-01-25 12:03:06 +0000

    chore: remove react-ui packages from projec

--- packages/react-ui/.storybook/coopTheme.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming/create';
-import Logo from './coop-logo.svg';
-
-export default create({
-  base: 'light',
-  brandTitle: 'Coop UI',
-  brandImage: Logo,
-});
--- packages/react-ui/src/Blockquote/src/index.mjs
+++
@@ -1,3 +0,0 @@
-import Blockquote from './Blockquote.jsx';
-
-export default Blockquote;
