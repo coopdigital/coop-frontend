6859777a6eea63e8aa53f4b1e533725e0077dfc4
Michael Chadwick <michael.chadwick@coopdigital.co.uk>
2022-02-10 09:10:41 +0000

    fix: prevent ms prefixing on flexbox rules

--- postcss.config.js
+++ postcss.config.js
@@ -13,7 +13,7 @@ module.exports = {
     }),
     require('postcss-nesting'),
     require('postcss-calc'),
-    require('autoprefixer'),
+    require('autoprefixer')({ flexbox: false }),
     require('cssnano'),
   ],
 };
