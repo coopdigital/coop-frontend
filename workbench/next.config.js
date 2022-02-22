1daad3045e90b1355631b5fe36362d1ba8866be0
Michael Chadwick <michael.chadwick@coopdigital.co.uk>
2022-02-22 14:35:22 +0000

    feat: enable workbench to use pcss files

--- workbench/next.config.js
+++ workbench/next.config.js
@@ -1,4 +1,23 @@
 module.exports = {
   reactStrictMode: true,
   basePath: '/workbench',
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.pcss/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+        },
+        {
+          loader: 'postcss-loader',
+        },
+      ],
+    });
+
+    return config;
+  },
 };
