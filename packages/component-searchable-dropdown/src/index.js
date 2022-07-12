e34e6dd817596efa5fbecbac8c09a4a6aea446b4
Omid Kashan <omid@antilong.com>
2022-07-12 14:08:17 +0100

    feat: remove old combobox lib code

--- packages/component-searchable-dropdown/src/index.js
+++ packages/component-searchable-dropdown/src/index.js
@@ -1,4 +1,3 @@
 import SearchableDropdown from './react/SearchableDropdown.jsx';
-import supportingFunction from './lib/SearchableDropdownLibrary.js';
 
-export { SearchableDropdown, supportingFunction };
+export { SearchableDropdown };
