6300b0713eac863d7f78c576a4254ef1d8884588
Omid Kashan <omid@antilong.com>
2022-07-12 15:31:08 +0100

    wip: render the combobox with no props yet

--- packages/component-searchable-dropdown/src/react/SearchableDropdown.jsx
+++ packages/component-searchable-dropdown/src/react/SearchableDropdown.jsx
@@ -1,56 +1,32 @@
-import React, { useRef, useEffect } from 'react';
-import SearchableDropdownLibrary from '../lib/SearchableDropdownLibrary.js';
+import React from 'react';
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from '@reach/combobox';
+import '@reach/combobox/styles.css';
 
 const SearchableDropdown = () => {
-  const comboboxRef = useRef();
-
-  useEffect(() => {
-    new SearchableDropdownLibrary(comboboxRef.current).setup();
-  }, []);
-
   return (
-    <div ref={comboboxRef}>
-      <div data-combobox>
-        <label htmlFor="combobox">How you know them</label>
-        <input
-          name="combobox"
-          className="coop-form__input"
-          type="text"
-          placeholder="Select option"
-          data-input
-          data-testid="combobox-input"
-          role="combobox"
-          aria-controls="comboboxvalues"
-          aria-autocomplete="list"
-          aria-expanded="false"
-        />
-        <div style={{ display: 'none' }} data-dropdown data-testid="combobox-dropdown">
-          <ul id="comboboxvalues" role="listbox" data-list>
-            {/* These valuse would be mapped from a data prop */}
-            <li role="option" aria-selected="false" data-option>
-              Partner
-            </li>
-            <li role="option" aria-selected="false" data-option>
-              Spouse
-            </li>
-            <li role="option" aria-selected="false" data-option>
-              Parent
-            </li>
-            <li role="option" aria-selected="false" data-option>
-              Child
-            </li>
-            <li role="option" aria-selected="false" data-option>
-              Brother
-            </li>
-            <li role="option" aria-selected="false" data-option>
-              Grandchild
-            </li>
-            <li role="option" aria-selected="false" data-option>
-              Father
-            </li>
-          </ul>
-        </div>
-      </div>
+    <div className="coop-c-combobox">
+      <label htmlFor="relationship" id="relationshipLabel">
+        This person is my:
+      </label>
+      <Combobox openOnFocus aria-labelledby="relationshipLabel">
+        <ComboboxInput id="relationship" />
+        <ComboboxPopover>
+          <ComboboxList>
+            <ComboboxOption value="Child" />
+            <ComboboxOption value="Grandchild" />
+            <ComboboxOption value="Grandparent" />
+            <ComboboxOption value="Parent" />
+            <ComboboxOption value="Partner" />
+            <ComboboxOption value="Spouse" />
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
     </div>
   );
 };
